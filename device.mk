c431485114d6ccfb723d94ea3845cadd33d6a57c
nino <pilopilosimon1990@gmail.com>
2026-03-23 13:43:53 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,126 +16,101 @@
 
 DEVICE_PATH := device/tecno/TECNO-LI9
 
+# Enable Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 31
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 31
-PRODUCT_TARGET_VNDK_VERSION := 34
 
-# A/B
-AB_OTA_UPDATER := true
 ENABLE_VIRTUAL_AB := true
-TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
+AB_OTA_UPDATER := true
+
 AB_OTA_PARTITIONS += \
-    apusys \
-    audio_dsp \
     boot \
-    ccu \
-    dpm \
     dtbo \
-    gpueb \
-    gz \
     lk \
-    logo \
-    mcf_ota \
-    mcupm \
-    md1img \
-    mvpu_algo \
     odm \
     odm_dlkm \
-    pi_img \
-    preloader_raw \
     product \
-    scp \
-    spmfw \
-    sspm \
     system \
     system_ext \
-    tee \
-    vbmeta \
     vbmeta_system \
     vbmeta_vendor \
-    vcp \
     vendor \
     vendor_boot \
-    vendor_dlkm \
-    mi_ext
-    
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier \
-    checkpoint_gc
+    vendor_dlkm
 
+# A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
-    FILESYSTEM_TYPE_system=erofs \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=erofs \
-    POSTINSTALL_OPTIONAL_vendor=true
+PRODUCT_PROPERTY_OVERRIDES += ro.twrp.vendor_boot=true
 
-# Additional Target Libraries
-TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hardware.keymaster@4.1 \
-    android.hardware.graphics.common@1.0 \
-    libion \
-    libxml2
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.graphics.common@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
+# API
+PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_TARGET_VNDK_VERSION := 31
 
-# Bootctrl
+# Boot control HAL
 PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-service \
     android.hardware.boot@1.2-mtkimpl \
     android.hardware.boot@1.2-mtkimpl.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
-    bootctrl
-
-# Dynamic
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Drm
+    bootctl \
+	update_engine_client
+ 
+# Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
 
-# Health
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# Health Hal
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
-
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.1
-
-# Keymint
-PRODUCT_PACKAGES += \
-    android.hardware.security.keymint \
-    android.hardware.security.secureclock \
-    android.hardware.security.sharedsecret
 
 # Keystore2
 PRODUCT_PACKAGES += \
     android.system.keystore2
 
-# Mtk plpath utils
+# Update engine
 PRODUCT_PACKAGES += \
-    mtk_plpath_utils \
-    mtk_plpath_utils.recovery
+    update_engine \
+    update_engine_sideload \
+    update_verifier
 
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
-    fastbootd
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
-# Hidl Service
-PRODUCT_ENFORCE_VINTF_MANIFEST := true
+# Additional Libraries
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libkeymaster41 \
+    libpuresoftkeymasterdevice \
+    libion \
+    libxml2
 
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
